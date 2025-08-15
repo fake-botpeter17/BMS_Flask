c42a3db184ff52b7e0558f8d6286fd590722aab6
Peter <fake.botpeter17@gmail.com>
2025-08-15 15:58:57 +0530

    Added Basic Inventory Functionalities.

--- db/inventory.py
+++ db/inventory.py
@@ -1,14 +1,15 @@
-from json import dumps
-from connection import getDB
+from json import dump, load
 from pymongo.asynchronous.collection import AsyncCollection
+from typing import Iterable
+from os import makedirs
+from os.path import join
+from .connection import getDB
 from utils.types import Item
-# from utils.misc import run_periodically
-
+from utils.misc import run_periodically
 
 inventory: AsyncCollection = getDB()["items"]
 
-
-def process_items(items: map[Item]) -> dict[int, dict]:
+def process_items(items: Iterable[Item]) -> dict[int, dict]:
     result = {}
     for item in items:
         result[item.id] = item.model_dump()
@@ -17,10 +18,16 @@ def process_items(items: map[Item]) -> dict[int, dict]:
 
 def cache_items():
     items_res = inventory.find({}, {"_id": False})
-    items = map(Item, items_res)
+    items = map(lambda item: Item(**item), items_res)
     items_processed = process_items(items)
-    
-    print(dumps(items_processed, indent=4))
+    makedirs('resources', exist_ok=True)
+    with open(join('resources','inventory.json'), 'w') as f:
+        dump(items_processed, fp=f, indent=4)
 
 
-cache_items()
+def get_items():
+    with open(join('resources','inventory.json')) as f:
+        res = load(f)
+    return process_items(res)
+
+items_cacher_event, items_cacher_thread = run_periodically(cache_items)
