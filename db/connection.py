199d0cf69f49d2979645df55ec2a3e1391efc817
Peter <fake.botpeter17@gmail.com>
2025-08-15 15:24:04 +0530

    Added Error Handling and improved code quality.

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from pymongo.synchronous.database import Database
-from pymongo.errors import ConnectionFailure
+from utils.exceptions import DatabaseInitializationError
 from dotenv import load_dotenv
 from os import getenv
 
@@ -8,17 +8,14 @@ load_dotenv()
 
 try:
     client: MongoClient = MongoClient(getenv("DB_URL"))
-    print("Done!")
-except ConnectionFailure:
-    raise Exception()
+except Exception as e:
+    raise DatabaseInitializationError(f"Database Connection Failed: {e}")
 
 else:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
 
 
 db_name: str = getenv("DB_NAME")
 
 def getDB() -> Database:
-    print(db_name)
     return client[db_name]
