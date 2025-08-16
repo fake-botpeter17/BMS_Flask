fdecf52ef513bee1a0a12d8451b7ebfa1fa64bdc
Peter <fake.botpeter17@gmail.com>
2025-08-16 22:54:39 +0530

    Added more response types.

--- auth/responses.py
+++ auth/responses.py
@@ -1,9 +1,34 @@
 from flask import jsonify
-
+from .tokens import generate_token
+from utils.types import User
+import datetime
 
 def key_error():
     return jsonify({
             "error" : "expired_key",
             "expired": True,
+            "status": "failure",
             "message" : "The key you used has expired. Please request for a new one."
         }), 401
+
+def auth_failed():
+    return jsonify({
+        "error": "failed_auth",
+        "status": "failure",
+        "message": "User Credentials are invalid."
+    }), 401
+
+def user_authenticated(user: User):
+    return jsonify(
+            {
+                "success": True, 
+                "access_token": generate_token(user.uid),
+                "refresh_token": generate_token(user.uid, refresh=True),
+                "token_type": 'Bearer',
+                "iat": datetime.datetime.now(datetime.timezone.utc),
+                "expires_in": 30*60,
+                "refresh_expires_in": 7*24*60*60,
+                "user": user.model_dump(),
+                "message": "Authentication Successful"
+            }
+        ), 200
