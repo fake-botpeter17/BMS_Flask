fc8aae3b5fde6155c89a2b048d32816a5e806ff7
Peter <fake.botpeter17@gmail.com>
2025-08-17 01:48:05 +0530

    Modified decorator to add role to function params.

--- auth/decorators.py
+++ auth/decorators.py
@@ -1,12 +1,14 @@
 from functools import wraps
 from .tokens import verify_token
 from flask import request, jsonify
+from db.users import userIsAdmin
 
 def require_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get("Authorization")
-        if not token or not verify_token(token):
+        data = verify_token(token)
+        if not token or not data:
             return jsonify({"error": "Unauthorized"}), 401
-        return f(*args, **kwargs)
+        return f(*args, **kwargs | {'isAdmin' : userIsAdmin(data['user_id'])})
     return decorated
