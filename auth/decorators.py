a07e120a7e9bacc51a82baf4f369cfeee2e887dc
Peter <fake.botpeter17@gmail.com>
2025-08-17 02:26:38 +0530

    Handled Token Exceptions.

--- auth/decorators.py
+++ auth/decorators.py
@@ -2,12 +2,15 @@ from functools import wraps
 from .tokens import verify_token
 from flask import request, jsonify
 from db.users import userIsAdmin
+from .responses import token_missing
 
 def require_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get("Authorization")
-        data = verify_token(token)
+        if not token:
+            return token_missing()
+        data = verify_token(token.split(' ')[1])
         if not token or not data:
             return jsonify({"error": "Unauthorized"}), 401
         return f(*args, **kwargs | {'isAdmin' : userIsAdmin(data['user_id'])})
